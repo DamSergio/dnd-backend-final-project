bef6341884fcd930b7a9a5ff7103e4bce2757dcd
DamSergio <dam_sergioballesteros@riberadeltajo.es>
2024-06-18 17:50:12 +0200

    se me olvido quitar pytest

--- app/utils/db_connection.py
+++ app/utils/db_connection.py
@@ -1,16 +1,9 @@
-import pytest
-
 from motor.motor_asyncio import AsyncIOMotorClient
 from app.models.models_in_db import User, Character, Campaign
 from config import CONFIG
 
 from contextlib import asynccontextmanager
 from beanie import init_beanie
-
-
-@pytest.fixture(scope="session")
-def anyio_backend():
-    return "asyncio"
 
 
 @asynccontextmanager
